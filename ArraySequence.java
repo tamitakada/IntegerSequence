ab4981d65f5e561e717604b4fcc07b116d14140f
Tami Takada <tami@mytalos.com>
2020-12-07 13:41:12 -0500

    added arraysequence

---
+++ ArraySequence.java
@@ -0,0 +1,10 @@
+import java.util.NoSuchElementException;
+
+
+public class ArraySequence implements IntegerSequence{
+  int currentIndex;
+  int[] data;
+
+  /*Construct the sequence by copying values from the other array into the data array*/
+  public ArraySequence(int [] other){  }
+}
